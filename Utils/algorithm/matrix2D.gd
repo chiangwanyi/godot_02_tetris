47a4eb831d9d6649bef1a411067d5183b47ec1c3
chiangwanyi <chiangwanyi@163.com>
2023-12-06 20:12:55 +0800

    add_child_matrix

--- Utils/algorithm/matrix2D.gd
+++ Utils/algorithm/matrix2D.gd
@@ -59,3 +59,52 @@ func is_value_exclude(v1: Variant, v2: Variant) -> bool:
 	if v1 == 0 or v2 == 0:
 		return false
 	return true
+
+func print_matrix():
+	for i in range(y):
+		print(data[i])
+
+			
+# 添加子矩阵
+func add_child_matrix(child: Matrix2D, origin_offset: Vector2i) -> bool:
+	# 检查子矩阵是否超出当前矩阵范围
+	if origin_offset.x < 0 or origin_offset.x + child.x > x or origin_offset.y < 0 or origin_offset.y + child.y > y:
+		return false
+
+	# 检查子矩阵是否与现有子矩阵重叠
+	for existing_child_origin in child_origin_offset.keys():
+		var existing_child = child_origin_offset[existing_child_origin]
+		if not is_non_overlapping(existing_child, existing_child_origin, child, origin_offset):
+			return false
+
+	# 添加子矩阵
+	children.append(child)
+	child_origin_offset[origin_offset] = child
+
+	# 更新父矩阵值
+	for i in range(child.y):
+		for j in range(child.x):
+			set_value(origin_offset.x + j, origin_offset.y + i, child.get_value(j, i))	
+	return true
+
+# 判断两个子矩阵是否不重叠
+func is_non_overlapping(child1: Matrix2D, offset1: Vector2i, child2: Matrix2D, offset2: Vector2i) -> bool:
+	var x1 = offset1.x
+	var y1 = offset1.y
+	var x2 = offset2.x
+	var y2 = offset2.y
+
+	for i in range(child1.y):
+		for j in range(child1.x):
+			var x1_pos = x1 + j
+			var y1_pos = y1 + i
+
+			for k in range(child2.y):
+				for l in range(child2.x):
+					var x2_pos = x2 + l
+					var y2_pos = y2 + k
+
+					if x1_pos == x2_pos and y1_pos == y2_pos:
+						if is_value_exclude(child1.get_value(j, i), child2.get_value(l, k)):
+							return false
+	return true
