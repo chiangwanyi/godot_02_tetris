d5eb2b6b21b9d53603c99f4d6cc35e592e52846f
chiangwanyi <chiangwanyi@163.com>
2023-12-06 20:13:05 +0800

    test

--- Scripts/game/game_state_machine.gd
+++ Scripts/game/game_state_machine.gd
@@ -4,6 +4,19 @@ extends "res://Utils/state_machine/state_machine.gd"
 @onready var game_playing_state: IState = $Playing
 
 func _ready():
+	var base = Matrix2D.new(10, 10, 0)
+	var a = Matrix2D.new(3, 3, 0, [
+		[1, 1, 1],
+		[1, 0, 1],
+		[1, 1, 1]
+	])
+	var b = Matrix2D.new(2, 2, 0, [
+		[2, 2],
+		[2, 2]
+	])
+	base.add_child_matrix(a, Vector2i(0, 0))
+	base.add_child_matrix(b, Vector2i(3, 3))
+	base.print_matrix()
 	states_map = {
 		"ready": game_ready_state,
 		"playing": game_playing_state
